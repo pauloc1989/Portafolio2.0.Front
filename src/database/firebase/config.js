5f0e6954d210dc8a1e0650b0179a65bfb400cd48
Paulo Urbano Rivera <paulo.1989.dev@gmail.com>
2024-01-21 21:23:48 -0300

    Se agregan variables de entorno a datos sensibles

--- src/database/firebase/config.js
+++ src/database/firebase/config.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const config = {
-    apiKey: "AIzaSyAJtSxzI7vKGsAwgsRv0_NjmpK-YWFC8DI",
-    authDomain: "portafolio-277601.firebaseapp.com",
-    projectId: "portafolio-277601",
-    storageBucket: "portafolio-277601.appspot.com",
-    messagingSenderId: "674252396129",
-    appId: "1:674252396129:web:197a6f0043e51b211bfc20"
+    apiKey: process.env.REACT_APP_APIKEY, //"AIzaSyAJtSxzI7vKGsAwgsRv0_NjmpK-YWFC8DI",
+    authDomain: process.env.REACT_APP_AUTHDOMAIN, //"portafolio-277601.firebaseapp.com",
+    projectId: process.env.REACT_APP_PROJECTID, //"portafolio-277601",
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET, //"portafolio-277601.appspot.com",
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID, //"674252396129",
+    appId: process.env.REACT_APP_APPID //"1:674252396129:web:197a6f0043e51b211bfc20"
 };
 
 firebase.initializeApp(config);
